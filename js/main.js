972ed87c753fe6c47bc3a4820dd9846abe454ba0
VadimGotsuliak <150594955+VadikG@users.noreply.github.com>
2025-06-25 22:56:24 +0300

    8888

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const btnMenu = document.querySelector('.btn-menu');
-  const mobileMenu = document.querySelector('.mobile__menu');
-  const closeBtn = document.querySelector('.mobile__btn-menu');
+const mobileMenu = document.querySelector('.mobile__menu');
+const closeMenu = document.querySelector('.mobile__btn-menu');
 
-  btnMenu.addEventListener('click', () => {
-    mobileMenu.classList.add('active');
-  });
-
-  closeBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('active');
-  });
+btnMenu.addEventListener('click', () => {
+  mobileMenu.classList.add('active');
+});
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+});
