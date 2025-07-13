bf44244a513f5c6e4951eef7e1b2d3b71611b49c
VadimGotsuliak <150594955+VadikG@users.noreply.github.com>
2025-07-13 08:33:20 +0300

    уууууу

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const btnMenu = document.querySelector('.btn-menu');
 const mobileMenu = document.querySelector('.mobile__menu');
 const closeMenu = document.querySelector('.mobile__btn-menu');
+const menuItems = document.querySelectorAll('.menu__item-mobile');
 
 btnMenu.addEventListener('click', () => {
   mobileMenu.classList.add('active');
 });
+
 closeMenu.addEventListener('click', () => {
   mobileMenu.classList.remove('active');
+});
+
+// ⬇️ Закриваємо меню при кліку на будь-який пункт:
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
 });
